f2a3766499317a5a6a096470134efbc6cc707b29
My name <me@example.com>
2024-02-29 15:46:08 +0530

    edited settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -103,13 +103,14 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysqldb',
-        'USER': 'admin',
-        'PASSWORD': 'admin12345',
-        'HOST': 'mysqldb.ckz7bawd2dzf.ap-south-1.rds.amazonaws.com',
+        'NAME': os.environ.get('CHATDB'),
+        'USER': os.environ.get('CHATDBUSER'),
+        'PASSWORD': os.environ.get('CHATDBPASSWORD'),
+        'HOST': os.environ.get('CHATDBHOST'),
         'PORT': '3306',
     }
 }
+
 #sagar kadam
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
